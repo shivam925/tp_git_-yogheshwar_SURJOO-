2656249d6f5fdeadd6017338a00b13ff796920e8
shivam925 <shivamsurjoo17@gmail.com>
2022-07-20 18:50:34 +0400

    Update addTwoInt.py
    code pour faire l'utilisateur d'inserez le bon nombre

--- addTwoInt.py
+++ addTwoInt.py
@@ -3,14 +3,17 @@
 import sys
 
 def add(a, b):
-    x = a + b
-    return x
+    sum = a + b
+    print(sum)
 
 try: 
     a = int( sys.argv[1] )
     b = int( sys.argv[2] )
 except IndexError:
     print("Error")
+    a = int(input("Input the first number: "))
+    b = int(input("Input the second number: "))
+    add(a,b)
     raise(SystemExit)
 
-print(add(a,b))
+add(a,b)
